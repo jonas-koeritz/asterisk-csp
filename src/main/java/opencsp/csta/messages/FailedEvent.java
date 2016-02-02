cf8da347df4c6976c1e62a2fe1d806cd5a6de32e
Jonas Köritz <jonas.koeritz@gmail.com>
2016-02-02 14:20:27 +0100

    Basic implementation of more event classes.

---
+++ src/main/java/opencsp/csta/messages/FailedEvent.java
@@ -0,0 +1,37 @@
+package opencsp.csta.messages;
+
+import opencsp.csta.*;
+import opencsp.csta.xml.CSTAXmlSerializable;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class FailedEvent extends CSTAEvent implements CSTAXmlSerializable {
+    private CrossReferenceId monitorCrossRefID;
+    private Connection failedConnection;
+    private DeviceId failingDevice;
+    private DeviceId callingDevice;
+    private DeviceId calledDevice;
+    private DeviceId lastRedirectionDevice;
+    private EventCause cause;
+
+    public Element toXmlElement(Document doc, String tagName) {
+        Element e = doc.createElement(tagName);
+        e.appendChild(monitorCrossRefID.toXmlElement(doc));
+        e.appendChild(failedConnection.toXmlElement(doc, "failedConnection"));
+        e.appendChild(failingDevice.toXmlElement(doc, "failingDevice"));
+        e.appendChild(callingDevice.toXmlElement(doc, "callingDevice"));
+        e.appendChild(calledDevice.toXmlElement(doc, "calledDevice"));
+        if(lastRedirectionDevice != null) {
+            e.appendChild(lastRedirectionDevice.toXmlElement(doc, "lastRedirectionDevice"));
+        } else {
+            e.appendChild(doc.createElement("lastRedirectionDevice").appendChild(doc.createElement("notRequired")));
+        }
+        e.appendChild(cause.toXmlElement(doc));
+        return e;
+    }
+
+    public Element toXmlElement(Document doc) {
+        return toXmlElement(doc, "BridgedEvent");
+    }
+}
+
---
+++ src/main/java/opencsp/csta/messages/HeldEvent.java
@@ -0,0 +1,27 @@
+package opencsp.csta.messages;
+
+import opencsp.csta.*;
+import opencsp.csta.xml.CSTAXmlSerializable;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class HeldEvent extends CSTAEvent implements CSTAXmlSerializable {
+    private CrossReferenceId monitorCrossRefID;
+    private Connection heldConnection;
+    private DeviceId holdingDevice;
+    private EventCause cause;
+
+    public Element toXmlElement(Document doc, String tagName) {
+        Element e = doc.createElement(tagName);
+        e.appendChild(monitorCrossRefID.toXmlElement(doc));
+        e.appendChild(heldConnection.toXmlElement(doc, "heldConnection"));
+        e.appendChild(holdingDevice.toXmlElement(doc, "holdingDevice"));
+        e.appendChild(cause.toXmlElement(doc));
+        return e;
+    }
+
+    public Element toXmlElement(Document doc) {
+        return toXmlElement(doc, "BridgedEvent");
+    }
+}
+
