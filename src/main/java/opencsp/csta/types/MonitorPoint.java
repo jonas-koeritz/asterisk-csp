dac859fb473b071a63b09555b27ff5ecbb80fbd0
Jonas Köritz <jonas.koeritz@gmail.com>
2016-02-03 16:20:46 +0100

    Cross reference IDs should not be created here.

--- src/main/java/opencsp/csta/types/MonitorPoint.java
+++ src/main/java/opencsp/csta/types/MonitorPoint.java
@@ -5,33 +5,11 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 public class MonitorPoint {
-    private static List<Integer> usedCrossReferenceIds;
     private CrossReferenceId crossReferenceId;
     private Device monitoredDevice;
 
     public MonitorPoint(CrossReferenceId crossReferenceId, Device monitoredDevice) {
         this.crossReferenceId = crossReferenceId;
         this.monitoredDevice = monitoredDevice;
-    }
-
-    public MonitorPoint(Device monitoredDevice) {
-        this.crossReferenceId = createCrossReferenceId();
-        this.monitoredDevice = monitoredDevice;
-    }
-
-    /**
-     * Generate a unique cross reference id.
-     * @return Unique cross reference id
-     */
-    private CrossReferenceId createCrossReferenceId() {
-        int id = 0;
-        try {
-            id = Collections.max(usedCrossReferenceIds);
-        } catch (NoSuchElementException ex) {
-            System.out.println("No used CrossReferenceID found. Starting at 0");
-        }
-        id++;
-        usedCrossReferenceIds.add(id);
-        return new CrossReferenceId(id);
     }
 }
