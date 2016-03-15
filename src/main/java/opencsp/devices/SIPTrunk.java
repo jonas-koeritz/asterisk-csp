88f0f61ab9c558e104f0c295aa1153ddc5628f59
Jonas Köritz <jonas.koeritz@gmail.com>
2016-03-15 13:04:32 +0100

    Work on constructor.

--- src/main/java/opencsp/devices/SIPTrunk.java
+++ src/main/java/opencsp/devices/SIPTrunk.java
@@ -5,7 +5,10 @@ import opencsp.csta.types.Device;
 import opencsp.csta.types.DeviceCategory;
 
 public class SIPTrunk extends Device {
+    int maxChannels = 0;
+
     public SIPTrunk(int maxChannels) {
         this.category = DeviceCategory.NetworkInterface;
+        this.maxChannels = maxChannels;
     }
 }
